95dd36134ea145dbd3478be3d4bfc4d27f26209a
Rayssa Vilaça <62777486+RayVilaca@users.noreply.github.com>
2025-09-23 08:30:02 -0300

    Remove unnecessary blank lines in test file

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -9,19 +9,16 @@ def test_set_title():
     task.set_title("exercise")
     assert task.title == "exercise"
 
-
 @pytest.mark.unit
 def test_set_done():
     task = Task(title="study", done=False)
     task.set_done(True)
     assert task.done is True
 
-
 @pytest.mark.unit
 def test_to_dict():
     task = Task(title="study", done=False)
     assert task.to_dict() == {"id": None, "title": "study", "done": False}
-
 
 @pytest.mark.unit
 def test_activity_set_title():
@@ -29,13 +26,11 @@ def test_activity_set_title():
     task.set_title("exercise")
     assert task.title == "exercise"
 
-
 @pytest.mark.unit
 def test_activity_set_done():
     task = Activity(title="study", done=False)
     task.set_done(True)
     assert task.done is True
-
 
 @pytest.mark.unit
 def test_activity_to_dict():
